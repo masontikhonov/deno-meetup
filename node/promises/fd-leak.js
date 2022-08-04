84534f335d77e0a6e39206aa96c54c2a66f5d706
Zhenya Tikhonov <masontikhonov@gmail.com>
2022-08-04 22:44:40 +0400

    refactor: add stupid comments

--- node/promises/fd-leak.js
+++ node/promises/fd-leak.js
@@ -3,9 +3,13 @@
 import * as fs from 'fs';
 
 const openAndThrow = () => {
+  // <~~ “Your promises, they look like lies” ©️ Jared Leto ~~>
+
   fs.open('./package.json', 'r', (err, fd) => { //* ➡️ async
     throw new Error(`❗Leaked FD #${fd}❗`);
   });
 };
 
-setInterval(openAndThrow, 50);
+// openAndThrow();
+
+// setInterval(openAndThrow, 50);
